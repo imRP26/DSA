3e3367f0656354c46f601adda11c69fd1447a02f
JimHalpert26 <rahulpadhy.15.09.1996@gmail.com>
2022-05-27 01:23:15 +0530

    Sort of Monotonic Stack.

---
+++ leetcodeNormalcy/31.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+/*
+ * Question Link -> https://leetcode.com/problems/next-permutation/
+*/
+
+
+
+// Simple problem involving a similar concept to Monotonic Stacks.
+class Solution1 {
+    public void nextPermutation(int[] nums) {
+    	int n = nums.length, i = n - 2, j = n - 1;
+		if (n == 1)
+			return;
+        /*
+         * Reason why we start our operation from the back-end of the array is 
+         * place value.
+         * For example, for the number 13542, we have :-
+         * 13542 = 2 * 10^0 + 4 * 10^1 + 5 * 10^2 + 3 * 10^3 + 1 * 10^4
+         * Usually the next higher number involves moving a digit having a 
+         * greater magnitude to a place having a higher place value.
+         * Now here, since we're concerned with just the next greater value, 
+         * we (greedily) search from the back so that any manipulations can be 
+         * done in digit positions having a lesser place value.
+        */
+		while (i >= 0 && nums[i] >= nums[i + 1])
+			i--;
+		if (i == -1) {
+			Arrays.sort(nums);
+			return;
+		}
+		while (j > i && nums[j] <= nums[i])
+			j--;
+		int temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
+        Arrays.sort(nums, i + 1, n);
+    }
+}
---
+++ leetcodeNormalcy/556.java
@@ -0,0 +1,63 @@
+/*
+ * Question Link -> https://leetcode.com/problems/next-greater-element-iii/
+*/
+
+
+
+// Same concept as Leetcode 31 : Next Permutation
+class Solution1 {
+    public int nextGreaterElement(int n) {
+        char[] number = (n + "").toCharArray();
+		int len = number.length, i = len - 2, j = len - 1;
+		while (i >= 0 && number[i] >= number[i + 1])
+			i--;
+		if (i == -1)
+			return -1;
+		while (j > i && number[j] <= number[i])
+			j--;
+		number[i] ^= number[j];
+		number[j] ^= number[i];
+		number[i] ^= number[j];
+		i++;
+		j = len - 1;
+		while (i < j) {
+			number[i] ^= number[j];
+			number[j] ^= number[i];
+			number[i++] ^= number[j--];
+		}
+		long value = Long.parseLong(new String(number));
+		return (value <= Integer.MAX_VALUE) ? (int) value : -1;
+    }
+}
+
+
+
+// Just going through another way of writing the same return statement
+class Solution2 {
+    public int nextGreaterElement(int n) {
+        char[] number = (n + "").toCharArray();
+		int len = number.length, i = len - 2, j = len - 1;
+		while (i >= 0 && number[i] >= number[i + 1])
+			i--;
+		if (i == -1)
+			return -1;
+		while (j > i && number[j] <= number[i])
+			j--;
+		number[i] ^= number[j];
+		number[j] ^= number[i];
+		number[i] ^= number[j];
+		i++;
+		j = len - 1;
+		while (i < j) {
+			number[i] ^= number[j];
+			number[j] ^= number[i];
+			number[i++] ^= number[j--];
+		}
+		try {
+			return Integer.valueOf(String.valueOf(number));
+		}
+		catch (NumberFormatException e) {
+			return -1;
+		}
+    }
+}
