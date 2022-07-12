fddb2deb1b93a35b36315780de963830feb8d7e3
JimHalpert26 <rahulpadhy.15.09.1996@gmail.com>
2022-07-12 17:20:59 +0530

    Boyer Moore + Divide-and-Conquer

--- intraCollegeContests/contest29/169.java
+++ intraCollegeContests/contest29/169.java
@@ -22,3 +22,37 @@ class Solution {
         return result;
     }
 }
+
+
+
+// Inefficient Solution using Divide-and-Conquer
+class Solution2 {
+
+    int countInRange(int[] nums, int num, int low, int high) {
+        int count = 0;
+        for (int i = low; i <= high; i++) {
+            if (nums[i] == num)
+                count++;
+        }
+        return count;
+    }
+
+    int recurse(int[] nums, int low, int high) {
+        // Base case - the only element in an array of size 1 is the majority element
+        if (low == high)
+            return nums[low];
+        int mid = low + (high - low) / 2, left = recurse(nums, low, mid), 
+            right = recurse(nums, mid + 1, high);
+        // if the 2 halves agree on the majority element, return it
+        if (left == right)
+            return left;
+        // otherwise, count each element and return the winner
+        int leftCount = countInRange(nums, left, low, high);
+        int rightCount = countInRange(nums, right, low, high);
+        return leftCount > rightCount ? left : right;
+    }
+
+    public int majorityElement(int[] nums) {
+        return recurse(nums, 0, nums.length - 1);
+    }
+}
