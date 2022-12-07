2c6e279190aad42c9bee1619f9c3cf36530b8758
JimHalpert26 <rahulpadhy.15.09.1996@gmail.com>
2022-12-07 16:13:22 +0530

    Brute + 2-Pointers + Stacks + DP

--- leetcode/32.java
+++ leetcode/32.java
@@ -39,20 +39,44 @@ class Solution1 {
 
 
 /*
- * dp[i] = length of the longest valid substring ending at the i'th index
- * If s[i] == '(', then dp[i] = 0, since no string ending with '(' can be 
- * considered to be valid.
- * If s[i] == ')' and s[i - 1] = '(', then dp[i] = dp[i - 2] + 2.
- * If s[i] == ')' and s[i - 1] = ')' and if s[i - dp[i - 1] - 1] = '(', then 
- * dp[i] = dp[i - 1] + dp[i - dp[i - 1] - 2] + 2.
+ * https://leetcode.com/problems/longest-valid-parentheses/solutions/127609/longest-valid-parentheses/comments/201028
+ * https://leetcode.com/problems/longest-valid-parentheses/solutions/14132/o-n-solution-constant-o-1-space-no-dp-no-stack/
+ * (Consider the example "(()" for understanding as to why traversals from both the sides is important)
  */
+class Solution2 {
+    public int longestValidParentheses(String s) {
+        int leftBrackets = 0, rightBrackets = 0, result = 0, len = s.length();
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) == '(')
+                leftBrackets++;
+            else
+                rightBrackets++;
+            if (leftBrackets == rightBrackets)
+                result = Math.max(result, 2 * rightBrackets);
+            else if (leftBrackets < rightBrackets)
+                leftBrackets = rightBrackets = 0;
+        }
+        leftBrackets = rightBrackets = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            if (s.charAt(i) == ')')
+                rightBrackets++;
+            else   
+                leftBrackets++;
+            if (rightBrackets == leftBrackets)
+                result = Math.max(result, 2 * leftBrackets);
+            else if (leftBrackets > rightBrackets)
+                leftBrackets = rightBrackets = 0;
+        }
+        return result;
+    }
+}
 
 
 
 /*
  * Referenced from -> https://www.youtube.com/watch?v=zhsjU1Bh9jQ
  */
-class Solution2 {
+class Solution3 {
     public int longestValidParentheses(String s) {
         Stack<Integer> stack = new Stack<>();
         int len = s.length(), result = 0, index = -1;
@@ -67,6 +91,42 @@ class Solution2 {
             index = i;
         }
         result = Math.max(result, len - index - 1);
+        return result;
+    }
+}
+
+
+
+/*
+ * DP Based Solution, to be referred from :-
+ * https://leetcode.com/problems/longest-valid-parentheses/solutions/127609/longest-valid-parentheses/
+ * https://leetcode.com/problems/longest-valid-parentheses/solutions/14135/dp-code-with-no-ugly-conditions-check/
+ * https://leetcode.com/problems/longest-valid-parentheses/solutions/14136/my-elegant-c-dp-solution-with-detailed-explanation/
+ */
+class Solution4 {
+    public int longestValidParentheses(String s) {
+        s = '*' + s;
+        int len = s.length(), result = 0;
+        int[] dp = new int[len];
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(')
+                    dp[i] = dp[i - 2] + 2;
+                else {
+                    /*
+                     * if current char is ')' and the 1st one before the last streak of valid 
+                     * parentheses is '(', we add :- 
+                     * (1) last streak before the new pair.
+                     * (2) the streak inside the new pair.
+                     * (3) the new pair.
+                     */
+                    int prev = i - dp[i - 1] - 1;
+                    if (s.charAt(prev) == '(')
+                        dp[i] = dp[prev - 1] + dp[i - 1] + 2;
+                }
+                result = Math.max(result, dp[i]);
+            }
+        }
         return result;
     }
 }
