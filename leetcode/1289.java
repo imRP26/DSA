40382aa3f1f18d8fc46006531bf2a1a810f8cd0e
Rahul Padhy <104157969+imRP26@users.noreply.github.com>
2022-12-13 12:13:51 +0530

    Ad-Hoc, not Hard

--- leetcode/1289.java
+++ leetcode/1289.java
@@ -1,7 +1,27 @@
+import java.util.*;
+
 /*
  * https://leetcode.com/problems/minimum-falling-path-sum-ii/
  */
 
 
-
-// 
+// Easiest Hard problem
+class Solution {
+    public int minFallingPathSum(int[][] grid) {
+        int n = grid.length, result = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int minVal = Integer.MAX_VALUE;
+                for (int k = 0; k < n; k++) {
+                    if (k == j)
+                        continue;
+                    minVal = Math.min(minVal, grid[i - 1][k]);
+                }
+                grid[i][j] += minVal;
+            }
+        }
+        for (int i = 0; i < n; i++)
+            result = Math.min(result, grid[n - 1][i]);
+        return result;
+    }
+}
