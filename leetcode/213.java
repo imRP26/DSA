e3a65277904f3e4d1f6b8ff938c07c472aa9bd6c
Rahul Padhy <104157969+imRP26@users.noreply.github.com>
2022-12-14 12:05:46 +0530

    Greedy, Tricky

--- leetcode/213.java
+++ leetcode/213.java
@@ -1,14 +1,19 @@
+import java.util.*;
+
+
 /*
  * Just be greedy and explore the 2 choices that you have...
+ * Reference -> 
+ * https://leetcode.com/problems/house-robber-ii/solutions/751859/house-robber-ii/?orderBy=most_votes
  */
 class Solution {
     
     public int robAmount(int[] nums, int startIndex) {
-		int n = nums.length, endIndex = (startIndex == 0) ? n - 2 : n - 1, rob1 = 0, rob2 = 0;
+		int n = nums.length, endIndex = (startIndex == 0) ? n - 2 : n - 1, robber1 = 0, robber2 = 0;
 		for (int i = startIndex; i <= endIndex; i++) {
-			int temp = Math.max(rob1 + nums[i], rob2);
-			rob1 = rob2;
-			rob2 = temp;
+			int temp = Math.max(robber1 + nums[i], robber2);
+			robber1 = robber2;
+			robber2 = temp;
 		}
 		return rob2;
 	}
