55fa918f13413be7fab229637be7e686bd6de60b
JimHalpert26 <rahulpadhy.15.09.1996@gmail.com>
2022-12-28 16:09:50 +0530

    Priority Queue, Easy

---
+++ leetcode/1962.java
@@ -0,0 +1,28 @@
+/*
+ * https://leetcode.com/problems/remove-stones-to-minimize-the-total/
+ */
+
+
+
+/*
+ * https://leetcode.com/problems/remove-stones-to-minimize-the-total/solutions/2636179/remove-stones-to-minimize-the-total/?orderBy=most_votes
+ */
+class Solution {
+    public int minStoneSum(int[] piles, int k) {
+        PriorityQueue<Integer> minPQ = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] != 0)
+                minPQ.add(piles[i]);
+        }
+        int minSum = 0;
+        while (k-- > 0 && !minPQ.isEmpty()) {
+            int x = minPQ.poll();
+            x -= x / 2;
+            if (x != 0)
+                minPQ.add(x);
+        }
+        while (!minPQ.isEmpty())
+            minSum += minPQ.poll();
+        return minSum;
+    }
+}
