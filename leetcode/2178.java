e491022779c5d4e46114daa871d980e2c94803f3
Rahul Padhy <104157969+imRP26@users.noreply.github.com>
2023-04-03 23:46:04 +0530

    Greedy, Tricky-ish

---
+++ leetcode/2178.java
@@ -0,0 +1,35 @@
+/*
+ * https://leetcode.com/problems/maximum-split-of-positive-even-integers/
+ */
+
+
+
+/*
+ * I AM NOT NON-TECHNICAL FFS!!!
+ */
+class Solution {
+    public List<Long> maximumEvenSplit(long finalSum) {
+        List<Long> result = new ArrayList<>();
+        if (finalSum % 2 != 0)
+            return result;
+        /*
+         * listSum -> sum of the elements present in the final list
+         * sum -> basically the even elements of the list, starting from 2
+         * cumulativeSum -> as a marker for not adding elements to the list beyond a certain point
+         */
+        long sum = 2, cumulativeSum = 2, listSum = 0;
+        while (cumulativeSum <= finalSum) {
+            result.add(sum);
+            listSum += sum;
+            sum += 2;
+            cumulativeSum += sum;
+        }
+        /*
+         * Upon verifying, got to know that the final list will always be of the form - 
+         * {2, 4, 6, 8, ..., X} 
+         */
+        if (listSum < finalSum)
+            result.set(result.size() - 1, result.get(result.size() -1) + finalSum - listSum);   
+        return result; 
+    }
+}
