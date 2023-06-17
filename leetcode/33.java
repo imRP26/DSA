94d1a05029c7a95df8aad0d0222aa702c30e093f
Rahul Padhy <104157969+imRP26@users.noreply.github.com>
2023-06-17 19:06:08 +0530

    BinarySearch, Tricky

--- leetcode/33.java
+++ leetcode/33.java
@@ -1,61 +1,34 @@
 /*
- * https://leetcode.com/problems/edit-distance/
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/
  */
 
 
 
 /*
- * Refer the approaches from Striver's video.
- */ 
+ * Refer the approach from Striver's explanation!
+ */
 class Solution {
-
-    private int memoization(int i1, int i2, int[][] dp, String s1, String s2) {
-        if (i1 < 0)
-            return 1 + i2;
-        if (i2 < 0)
-            return 1 + i1;
-        if (dp[i1][i2] != -1)
-            return dp[i1][i2];
-        if (s1.charAt(i1) == s2.charAt(i2))
-            return dp[i1][i2] = memoization(i1 - 1, i2 - 1, dp, s1, s2);
-        int insertionCost = memoization(i1, i2 - 1, dp, s1, s2);
-        int deletionCost = memoization(i1 - 1, i2, dp, s1, s2);
-        int replacementCost = memoization(i1 - 1, i2 - 1, dp, s1, s2);
-        return dp[i1][i2] = 1 + Math.min(Math.min(insertionCost, deletionCost), replacementCost);
-    }
-
-    public int minDistance(String word1, String word2) {
-        int len1 = word1.length(), len2 = word2.length();
-        /*
-        int[][] dp = new int[len1 + 1][len2 + 1];
-        //for (int[] row : dp)
-        //    Arrays.fill(row, -1);
-        //return memoization(len1 - 1, len2 - 1, dp, word1, word2);
-         for (int i = 0; i <= len1; i++) {
-             for (int j = 0; j <= len2; j++) {
-                 if (i == 0 || j == 0)
-                    dp[i][j] = i + j;
-                else if (j == 0)
-                    dp[i][j] = i;
-                else if (word1.charAt(i - 1) == word2.charAt(j - 1))
-                    dp[i][j] = dp[i - 1][j - 1];
+    public int search(int[] nums, int target) {
+        int n = nums.length, low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            // when the left half is sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid])
+                    high = mid - 1;
                 else
-                    dp[i][j] = 1 + Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1]));
-             }
-         }
-         */
-         int[][] dp = new int[2][len2 + 1];
-        for (int i = 0; i <= len1; i++) {
-            for (int j = 0; j <= len2; j++) {
-                if (i == 0 || j == 0)
-                    dp[i % 2][j] = i + j;
-                else if (word1.charAt(i - 1) == word2.charAt(j - 1))
-                    dp[i % 2][j] = dp[1 - (i % 2)][j - 1];
+                    low = mid + 1;
+            }
+            // when the right half is sorted
+            else if (nums[mid] <= nums[high]) {
+                if (nums[mid] <= target && target <= nums[high])
+                    low = mid + 1;
                 else
-                    dp[i % 2][j] = 1 + Math.min(dp[i % 2][j - 1], Math.min(dp[1 - (i % 2)][j], dp[1 - (i % 2)][j - 1]));
+                    high = mid - 1;
             }
         }
-         return dp[len1][len2];
+        return -1;
     }
 }
- 
