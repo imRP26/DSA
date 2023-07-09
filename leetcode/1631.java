e135ce8abdb2123e20d73073b5b18154fb99302a
Rahul Padhy <104157969+imRP26@users.noreply.github.com>
2023-07-09 16:54:18 +0530

    BFS + BinarySearch,DFS + BinarySearch, DSU, Dijkstra's

--- leetcode/1631.java
+++ leetcode/1631.java
@@ -104,3 +104,67 @@ class Solution {
         return diffMatrix[rows - 1][cols - 1];
     }
 }
+
+
+
+/*
+ * Approach of DSU from -> 
+ * https://leetcode.com/problems/path-with-minimum-effort/solutions/1036518/java-3-clean-codes-dijkstra-s-algo-union-find-binary-search/
+ */
+class Solution {
+
+    int rows, cols;
+    int[] parent, rank;
+
+    int findParent(int x) {
+        while (x != parent[x]) {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+
+    void union(int x, int y) {
+        int xPar = findParent(x), yPar = findParent(y);
+        if (xPar == yPar)
+            return;
+        if (rank[xPar] >= rank[yPar]) {
+            rank[xPar] += rank[yPar];
+            parent[yPar] = parent[xPar]; 
+        }
+        else {
+            rank[yPar] += rank[xPar];
+            parent[xPar] = parent[yPar];
+        }
+    }
+
+    public int minimumEffortPath(int[][] heights) {
+        rows = heights.length;
+        cols = heights[0].length;
+        parent = new int[rows * cols];
+        for (int i = 0; i < rows * cols; i++)
+            parent[i] = i;
+        rank = new int[rows * cols];
+        Arrays.fill(rank, 1);
+        List<int[]> edges = new ArrayList<>();
+        Set<Integer> vis = new HashSet<>();
+        for (int i = 0; i < rows; i++) {
+        	for (int j = 0; j < cols; j++) {
+        		int currCell = i * cols + j;
+        		if (i + 1 < rows) {
+        			int nextCell = (i + 1) * cols + j;
+        			edges.add(new int[] {currCell, nextCell, Math.abs(heights[i][j] - heights[i + 1][j])});
+        		}
+        		if (j + 1 < cols) {
+        			int nextCell = i * cols + (j + 1);
+        			edges.add(new int[] {currCell, nextCell, Math.abs(heights[i][j] - heights[i][j + 1])});
+        		}
+        	}
+        }
+        Collections.sort(edges, (a, b) -> a[2] - b[2]);
+        int srcNode = 0, destNode = rows * cols - 1, i = 0;
+        while (findParent(srcNode) != findParent(destNode))
+        	union(edges.get(i)[0], edges.get(i++)[1]);
+        return i == 0 ? i : edges.get(i - 1)[2];
+    }
+}
