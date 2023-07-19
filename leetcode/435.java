8507630f0a682b3c7a70f0ef0b96f04f0164d5f8
Rahul Padhy <104157969+imRP26@users.noreply.github.com>
2023-07-19 13:01:47 +0530

    Greedy, Sorting, MEDIUM

--- leetcode/435.java
+++ leetcode/435.java
@@ -24,3 +24,29 @@ class Solution {
         return result;
     }
 }
+
+
+
+/*
+ * Approach from LC Official Editorial!
+ * Sorting greedily wrt the end-time of an interval - this ensures that not many intervals 
+ * afterwards will have conflicts!
+ */
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        //Arrays.sort(intervals, (a, b) -> (a[1] == b[1]) ? (a[0] - b[0]) : (a[1] - b[1]));
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[1] == b[1])
+                return a[0] - b[0];
+            return a[1] - b[1];
+        });
+        int res = 0, t = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < t)
+                res++;
+            else
+                t = intervals[i][1];
+        }
+        return res;
+    }
+}
