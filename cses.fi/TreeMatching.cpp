88a110d9f6d6b23ed11ba46bb9ba0a630a13fff3
Rahul Padhy <104157969+imRP26@users.noreply.github.com>
2023-07-24 16:06:12 +0530

    DP on Trees

--- cses.fi/TreeMatching.cpp
+++ cses.fi/TreeMatching.cpp
@@ -4,6 +4,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+ * THE BIG EXPLANATION :- (wrt the example tree given below)
+ *                                                      (1)
+ * 													 /	 \    \
+ * 													/	 \     \
+ *                                                (2)   (3)   (4)
+ * 												      /  \  \
+ * 													 /	 \   \
+ *  												(5) (6) (7)
+ * 
+ * TFO :- The maximum number of edges where each node is an endpoint of atmaax 1 edge.
+ * 
+ * DP States :-
+ * dp(i, 0) = answer for the subtree rooted at node 'i' such that there's no edge between the node 'i' and 
+ * 			  any of its children!
+ * dp(i, 1) = answer for the subtree rooted at node 'i' such that there's an edge between the node 'i' and 
+ * 			  only 1 of its children!
+ * The Final answer = max(dp[1][0], dp[1][1]) - assuming that the given tree is being rooted at node '1'.
+ * 
+ * Base Case :-
+ * dp[leaf][0] = dp[leaf][1] = 0.
+ * 
+ * DP Transitions when we're currently at node '1' :-
+ * dp[1][0] = max(dp[2][0], dp[2][1]) + max(dp[3][0], dp[3][1]) + max(dp[4][0], dp[4][1])
+ * => max(dp[3][0]) + max(dp[4][0]) = dp[1][0] - max(dp[2][0], dp[2][1])
+ * => dp[node][0] = summation over all children {max(dp[child][0], dp[child][1])}
+ * If we consider the edge (1)----(2) in the final answer, then 
+ * dp[1][1] = 1 + dp[2][0] + max(dp[3][0], dp[3][1]) + max(dp[4][0], dp[4][1])
+ * => dp[1][1] = 1 + dp[1][0] + dp[2][0] - max(dp[2][0], dp[2][1])
+ * => dp[node][1] = 1 + dp[node][0] + dp[child][0] - max(dp[child][0], dp[child][1]) 
+ */
+
 vector<vector<int> > graph, dp;
 
 int memoization(int node, int parent, int flag) {
