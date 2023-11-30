2cc76becc83fca86938ccdff935ecf90caab7aef
Rahul Padhy <104157969+imRP26@users.noreply.github.com>
2023-11-30 17:20:20 +0530

    Stack

--- leetcode/84.java
+++ leetcode/84.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 /*
  * https://leetcode.com/problems/largest-rectangle-in-histogram/
  */
@@ -10,45 +8,37 @@ import java.util.*;
  * 2 Pass approach using Stacks :- 
  * Find the index of the next smaller element on the left and on the right and 
  * then using the present index, find the area of the rectangle thus formed.
- * Video Link that can be referred :- 
- * https://www.youtube.com/watch?v=X0X6G-eWgQ8&list=PLgUwDviBIf0oSO572kQ7KCSvCUh1AdILj&index=11
+ * Referred from Striver's video!
  */
-class Solution1 {
-	public int largestRectangleArea(int[] heights) {
-		int numHeights = heights.length, result = 0;
-		Stack<Integer> stack = new Stack<>();
-		int[] leftSmaller = new int[numHeights];
-		int[] rightSmaller = new int[numHeights];
-		for (int i = 0; i < numHeights; i++) {
-			while (!stack.isEmpty() && heights[stack.peek()] >= heights[i])
-				stack.pop();
-			if (stack.isEmpty())
-				leftSmaller[i] = 0;
-			else
-				leftSmaller[i] = stack.peek() + 1;
-			stack.push(i);
-		}
-		while (!stack.isEmpty())
-			stack.pop();
-		for (int i = numHeights - 1; i >= 0; i--) {
-			while (!stack.isEmpty() && heights[stack.peek()] >= heights[i])
-				stack.pop();
-			if (stack.isEmpty())
-				rightSmaller[i] = numHeights - 1;
-			else
-				rightSmaller[i] = stack.peek() - 1;
-			stack.push(i);
-		}
-		for (int i = 0; i < numHeights; i++)
-			result = Math.max(result, heights[i] * (rightSmaller[i] - leftSmaller[i] + 1));
-		return result;
-	}
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length, result = Integer.MIN_VALUE;
+        int[] leftSmaller = new int[n], rightSmaller = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i])
+                stack.pop();
+            leftSmaller[i] = stack.isEmpty() ? 0 : stack.peek() + 1;
+            stack.push(i);
+        }
+        while (!stack.isEmpty())
+            stack.pop();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i])
+                stack.pop();
+            rightSmaller[i] = stack.isEmpty() ? n - 1 : stack.peek() - 1;
+            stack.push(i);
+        }
+        for (int i = 0; i < n; i++)
+            result = Math.max(result, heights[i] * (rightSmaller[i] - leftSmaller[i] + 1));
+        return result;
+    }
 }
 
 
 
 /*
  * 1 Pass Approach :-
- * 
+ * Referred from Striver's video!
  */
 
